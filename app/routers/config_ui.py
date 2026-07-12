16045b3c161a1add23819c28291e1f401c42093f
kesurof <kesurof@gmail.com>
2026-07-12 22:35:57 +0200

    Refactor code structure for improved readability and maintainability

--- app/routers/config_ui.py
+++ app/routers/config_ui.py
@@ -8,4 +8,11 @@ router = APIRouter()
 
 @router.get("/config", response_class=HTMLResponse)
 async def config_page(request: Request):
-    return templates.TemplateResponse(request, "config.html")
+    return templates.TemplateResponse(
+        request,
+        "config.html",
+        {
+            "radarr_naming_guide": "/static/config_arr/radarr.png",
+            "sonarr_naming_guide": "/static/config_arr/sonarr.png",
+        },
+    )
